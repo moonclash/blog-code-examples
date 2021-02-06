8810dc4fd3d80f451d5c57ae1f0ffcc71917871b
moonclash <emladenoff@gmail.com>
2021-02-06 11:50:49 +0000

    identation

--- gopi/api.go
+++ gopi/api.go
@@ -4,21 +4,21 @@ import "github.com/gin-gonic/gin"
 import "github.com/moonclash/blog-code-examples/gopi/database"
 
 func main() {
-	database.CreateDB()
-	DatabaseManager := database.New(nil)
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+  database.CreateDB()
+  DatabaseManager := database.New(nil)
+  router := gin.Default()
+  router.GET("/ping", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "pong",
+    })
+  })
 
-	router.GET("/fetch", func(c *gin.Context) {
-		DatabaseManager.RetrieveDefinition("test")
-	})
+  router.GET("/fetch", func(c *gin.Context) {
+    DatabaseManager.RetrieveDefinition("test")
+  })
 
-	router.POST("/create", func(c *gin.Context) {
-		DatabaseManager.InsertDefinition("test", "test definition")
-	})
-	router.Run()
+  router.POST("/create", func(c *gin.Context) {
+    DatabaseManager.InsertDefinition("test", "test definition")
+  })
+  router.Run()
 }
--- gopi/database/db_manager.go
+++ gopi/database/db_manager.go
@@ -1,54 +1,54 @@
 package database
 
 import (
-	"os"
-	"database/sql"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
+  "os"
+  "database/sql"
+  "fmt"
+  _ "github.com/mattn/go-sqlite3"
 )
 
 func checkErr(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
 
 func CreateDB() {
-	os.Create("./data.db")
-	db, err := sql.Open("sqlite3", "./data.db")
-	checkErr(err)
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `definitions` (`definition_id` INTEGER PRIMARY KEY AUTOINCREMENT, `short_name` VARCHAR(255), `long_name` VARCHAR(255))")
-	checkErr(err)
+  os.Create("./data.db")
+  db, err := sql.Open("sqlite3", "./data.db")
+  checkErr(err)
+  _, err = db.Exec("CREATE TABLE IF NOT EXISTS `definitions` (`definition_id` INTEGER PRIMARY KEY AUTOINCREMENT, `short_name` VARCHAR(255), `long_name` VARCHAR(255))")
+  checkErr(err)
 }
 
 type DBManager struct {
-	db *sql.DB
+  db *sql.DB
 }
 
 func New(db *sql.DB) DBManager {
-	_db := DBManager {db}
-	return _db
+  _db := DBManager {db}
+  return _db
 }
 
 
 func (_db DBManager) InsertDefinition(key string, definition string) {
-	db, err := sql.Open("sqlite3", "./data.db")
-	checkErr(err)
-	_db.db = db
-	insertQuery := "INSERT INTO definitions (short_name, long_name) VALUES (?, ?)"
-	statement, err := _db.db.Prepare(insertQuery)
-	checkErr(err)
-	statement.Exec(key, definition)
-	checkErr(err)
+  db, err := sql.Open("sqlite3", "./data.db")
+  checkErr(err)
+  _db.db = db
+  insertQuery := "INSERT INTO definitions (short_name, long_name) VALUES (?, ?)"
+  statement, err := _db.db.Prepare(insertQuery)
+  checkErr(err)
+  statement.Exec(key, definition)
+  checkErr(err)
 }
 
 func (_db DBManager) RetrieveDefinition(key string) {
-	db, err := sql.Open("sqlite3", "./data.db")
-	checkErr(err)
-	_db.db = db
-	string_query := fmt.Sprintf("select short_name, long_name from definitions where short_name = '%s'", key)
-	rows, err := _db.db.Query(string_query)
-	fmt.Println(rows)
-	checkErr(err)
+  db, err := sql.Open("sqlite3", "./data.db")
+  checkErr(err)
+  _db.db = db
+  string_query := fmt.Sprintf("select short_name, long_name from definitions where short_name = '%s'", key)
+  rows, err := _db.db.Query(string_query)
+  fmt.Println(rows)
+  checkErr(err)
 }

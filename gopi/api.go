fa83d75a9deaf157d651bfc47249a2fb08be5c34
moonclash <emladenoff@gmail.com>
2021-02-06 12:21:44 +0000

    implement in api

--- gopi/api.go
+++ gopi/api.go
@@ -4,21 +4,25 @@ import "github.com/gin-gonic/gin"
 import "github.com/moonclash/blog-code-examples/gopi/database"
 
 func main() {
-  database.CreateDB()
-  DatabaseManager := database.New(nil)
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
+	database.CreateDB()
+	DatabaseManager := database.New(nil)
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/fetch", func(c *gin.Context) {
+		short_name, long_name := DatabaseManager.RetrieveDefinition("test")
     c.JSON(200, gin.H{
-      "message": "pong",
+      "short_name": short_name,
+      "long_name": long_name,
     })
-  })
+	})
 
-  router.GET("/fetch", func(c *gin.Context) {
-    DatabaseManager.RetrieveDefinition("test")
-  })
-
-  router.POST("/create", func(c *gin.Context) {
-    DatabaseManager.InsertDefinition("test", "test definition")
-  })
-  router.Run()
+	router.POST("/create", func(c *gin.Context) {
+		DatabaseManager.InsertDefinition("test", "test definition")
+	})
+	router.Run()
 }
